2f0dbbaca71947324037fea6e302c355fd1c9ab1
Igor Farafonov <entertoinfo29@gmail.com>
2022-08-31 23:21:15 +0300

    update

--- js/renderProducts.js
+++ js/renderProducts.js
@@ -1,1 +1,50 @@
 const productsContainer = document.querySelector('#products-container')
+
+//Запускаем getProduct
+getProducts();
+
+//Асинхронная функция получения данных из файла products.json
+async function getProducts(){
+  //Получаем данные из products.json
+  const response = await fetch('./js/products.json');
+  //Парсим данные из JSON формата в JS 
+  const productsArray = await response.json();
+  //Запускаем ф-ю рендера (отображение товаров)
+  renderProducts(productsArray);
+}
+
+
+function renderProducts(productsArray){
+  productsArray.forEach(function(item){
+    const productHTML = `<div class="col-md-6">
+    <div class="card mb-4" data-id="${item.id}">
+      <img class="product-img" src="${item.imgSrc}">
+      <div class="card-body text-center">
+        <h4 class="item-title">${item.title}</h4>
+        <p><small data-items-in-box class="text-muted">6 шт.</small></p>
+
+        <div class="details-wrapper">
+          <!-- Счетчик -->
+          <div class="items counter-wrapper">
+            <div class="items__control" data-action="minus">-</div>
+            <div class="items__current" data-counter>1</div>
+            <div class="items__control" data-action="plus">+</div>
+          </div>
+          <!-- //Счетчик -->
+
+          <div class="price">
+            <div class="price__weight">${item.weight}</div>
+            <div class="price__currency">${item.price} $</div>
+          </div>
+        </div>
+
+        <button data-cart type="button" class="btn btn-block btn-outline-warning">
+          + в корзину
+        </button>
+
+      </div>
+    </div>
+  </div>`;
+  productsContainer.insertAdjacentHTML('beforeend', productHTML);
+  })
+}
